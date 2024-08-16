72fd159df05cfacaa49d740f8d5766bc403fdab2
vpasemko <vpasemko@gmail.com>
2024-08-16 17:31:49 +0300

    Homework 3 done

---
+++ homework-3-promises-and-worker-threads/task1.js
@@ -0,0 +1,22 @@
+async function makeFewApiCalls(...links) {
+    let responses  = await Promise.all(links.map((link)=> fetch(link)))
+    // console.log(responses)
+    let jsonedResult = await Promise.all(responses.map(response => response.json()));
+    return jsonedResult;
+}
+
+// positive scenario
+
+makeFewApiCalls(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2",
+    "https://jsonplaceholder.typicode.com/posts/3"
+).then(data => console.log(data)).catch(error => console.error('Error:', error));
+
+// negative scenario
+
+makeFewApiCalls(
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2",
+    "notexistedlinkthatshoulcauseanerror"
+).then(data => console.log(data)).catch(error => console.error('Error:', error));
